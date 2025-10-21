f746dd89ac49cb188fe7b513f44d112f32df8001
Yara <yaramohamed1886757@icloud.com>
2025-10-21 23:39:51 +0300

    genrate use case

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,6 +1,9 @@
+import 'package:bookly/core/errors/failure.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-Future<List<BookEntity>> fetchFeaturesBook();
-Future<List<BookEntity>> fetchNewsBook();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturesBook();
+
+  Future<Either<Failure, List<BookEntity>>> fetchNewsBook();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:bookly/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'features/splash/presentation/views/splash_view.dart';
 
 void main() {
   runApp(const MyApp());
