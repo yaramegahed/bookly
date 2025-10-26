8ea274eeb5c8cb25f8e20825a1bcade50d443c9d
Yara <yaramohamed1886757@icloud.com>
2025-10-26 21:51:17 +0300

    implement featured books cubit

--- lib/features/home/presentation/manger/featured_books_cubit.dart
+++ lib/features/home/presentation/manger/featured_books_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly/features/home/domain/useCases/fetch_features_books_use_case.dart';
 import 'package:meta/meta.dart';
 
 import '../../domain/entities/book_entity.dart';
@@ -6,5 +7,17 @@ import '../../domain/entities/book_entity.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.fetchFeaturesBooksUseCase)
+      : super(FeaturedBooksInitial());
+  final FetchFeaturesBooksUseCase fetchFeaturesBooksUseCase;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await fetchFeaturesBooksUseCase.call();
+    result.fold((failure) {
+      emit(FeaturedBooksFailure(failure.message));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
