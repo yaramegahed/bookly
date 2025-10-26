2dff196830aa539affd203712843dcc78794cfcc
Yara <yaramohamed1886757@icloud.com>
2025-10-26 21:41:10 +0300

    create books cubit

---
+++ lib/features/home/presentation/manger/featured_books_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import '../../domain/entities/book_entity.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+}
